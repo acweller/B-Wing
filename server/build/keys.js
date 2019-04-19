ae0d63e24c5f2472fa1057da7c1ebafc97ae0f5a
Anderson Weller <acweller@gmail.com>
2019-04-19 13:28:36 -0300

    Criação do Backend
    Conexão ao BD e codificação do Backend.

---
+++ server/build/keys.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        host: 'localhost',
+        user: 'phpmyadmin',
+        password: 'phpmyadmin',
+        database: 'ng_games'
+    }
+};
