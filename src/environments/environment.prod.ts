778def401cdc411983bb707808de1df92037017d
Anderson Weller <acweller@gmail.com>
2019-08-23 12:56:14 -0300

    Atualização do arquivo de conf do Travis

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api_uri: 'http://192.168.0.113:3000/vgame'
+  api_uri: 'https://b-wing.herokuapp.com:3000/vgame',
+  api_uri_local: 'http://192.168.0.113:3000/vgame'
 };
